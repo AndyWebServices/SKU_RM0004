36473903b4875f1f10b8efac618300f44afc5e4e
Andrew M. Zhang <andy@andywebservices.com>
2025-05-31 20:22:12 -0500

    Fix formatting, reduce redraws

--- project/display.c
+++ project/display.c
@@ -3,30 +3,28 @@ Demo for ssd1306 i2c driver for  Raspberry Pi
 ******/
 #include <stdio.h>
 #include "st7735.h"
-#include "time.h"
 #include <unistd.h>
 
 
+int main(void) {
+    uint8_t symbol = 0;
 
-int main(void) 
-{
-	uint8_t symbol = 0;
-	
-	if(lcd_begin())      //LCD Screen initialization
-	{
-		return 0;
-	}
-	sleep(1);
-	while(1)
-	{
-		lcd_display(symbol);
-		sleep(1);
-        sleep(1);
-		symbol++;
-		if(symbol==4)
-        {
-          symbol=0;
+    if (lcd_begin()) //LCD Screen initialization
+    {
+        return 0;
+    }
+    sleep(1);
+
+    lcd_fill_screen(ST7735_BLACK);
+    lcd_fill_rectangle(0, 20, ST7735_WIDTH, 5, ST7735_BLUE);
+
+    while (1) {
+        lcd_display(symbol);
+        sleep(2);
+        symbol++;
+        if (symbol == 4) {
+            symbol = 0;
         }
-	}
-	return 0;
+    }
+    return 0;
 }
